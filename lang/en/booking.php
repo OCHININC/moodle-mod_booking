c5d01d58543d51dad5be5eb8bad79a0cec123702
Michael <mikeaveli007@hotmail.com>
2024-11-27 12:08:02 -0800

    Add install member booking condition [ELLA-11908]

--- lang/en/booking.php
+++ lang/en/booking.php
@@ -195,6 +195,7 @@ $string['boactions_desc'] = "Booking actions after booking are still an EXPERIME
 But do not use them in a productive environment yet!";
 $string['boactionselectuserprofilefield'] = "Choose profile field";
 $string['boactionuserprofilefieldvalue'] = 'Value';
+$string['bocondinstallmembers'] = 'Install Members';
 $string['bocondallowedtobookininstance'] = 'Booking should be possible without special capability';
 $string['bocondallowedtobookininstanceanyways'] = "Users are allowed to book without the capability '<b>mod/booking:choose</b>'.<br>
 <div class='text-danger'>Note: Both this checkbox and the upper checkbox need to be checked if you want to activate this.</div>";
